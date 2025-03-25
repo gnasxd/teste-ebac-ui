98c0cd2aa7b81ce43065fe2f81f43c4df81c69e2
Guilherme Augusto <guibiston@gmail.com>
2025-03-24 22:03:05 -0300

    Produtos e listas

--- cypress/e2e/3- loja-ebac/produtos.cy.js
+++ cypress/e2e/3- loja-ebac/produtos.cy.js
@@ -5,8 +5,17 @@ describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
-    
+
     it('Deve selecionar um produto da lista', () => {
+        cy.get(' .product-block ')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Apollo Running Short')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+
 
     });
 });
