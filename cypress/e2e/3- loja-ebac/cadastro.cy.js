d609d6cb3891696962c7e1f130ff9504c8a9ef9e
Guilherme Augusto <guibiston@gmail.com>
2025-03-24 21:47:34 -0300

    faker e hooks

--- cypress/e2e/3- loja-ebac/cadastro.cy.js
+++ cypress/e2e/3- loja-ebac/cadastro.cy.js
@@ -19,7 +19,7 @@ describe('Funcionalidade: Cadastro', () => {
         cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
     });
 
-    it.only('Deve completar o cadastro com sucesso - Usando variáveis', () => {
+    it('Deve completar o cadastro com sucesso - Usando variáveis', () => {
         var nome = faker.person.firstName()
         var email = faker.internet.email(nome)
         var sobrenome = faker.person.lastName()
